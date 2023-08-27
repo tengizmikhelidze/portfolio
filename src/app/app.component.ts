18b266042ed19d1c3d3d54f73470e9a97da29cd4
Tengiz Mikhelidze <mixelidzet@gmail.com>
2023-08-28 03:15:24 +0400

    fix - firebase call to typecheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
-import { Component } from '@angular/core';
-import {FirebaseService} from "../shared/firebase/firebase.service";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private firebaseService: FirebaseService) {
-    this.firebaseService.get("words").subscribe((data)=>{console.log(data)})
-  }
-  title = 'portfolio';
-}
+export class AppComponent {}
--- src/shared/firebase/firebase.service.ts
+++ src/shared/firebase/firebase.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {collection, collectionData, Firestore} from "@angular/fire/firestore";
-import {CollectionReference, DocumentData} from "@firebase/firestore";
 import {Observable} from "rxjs";
 
 @Injectable({
@@ -9,7 +8,7 @@ import {Observable} from "rxjs";
 export class FirebaseService {
   constructor(private firestore: Firestore) { }
 
-  get(path: string): Observable<DocumentData[]>{
-    return collectionData(collection(this.firestore, path))
+  get<T>(path: string): Observable<T[]>{
+    return collectionData(collection(this.firestore, path)) as Observable<T[]>
   }
 }
