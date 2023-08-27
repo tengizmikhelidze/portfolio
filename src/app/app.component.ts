4f0df3c837a28b8a1d1e1ae0cf7db0ae84c7ba45
Tengiz Mikhelidze <mixelidzet@gmail.com>
2023-08-28 03:54:06 +0400

    feat - styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,12 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    this.setDevicePreferredTheme()
+  }
+  private setDevicePreferredTheme(){
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+    document.body.classList.toggle('dark', prefersDark.matches)
+  }
+}
