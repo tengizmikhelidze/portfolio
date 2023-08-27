dd90ba04f3febb89bffb66a69f54b6000e876a1f
Tengiz Mikhelidze <mixelidzet@gmail.com>
2023-08-28 02:39:21 +0400

    feat - add firebase connection

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'portfolio-10g',
+    appId: '1:51483704937:web:b3abfa94af8079b4deacf8',
+    storageBucket: 'portfolio-10g.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyCVfc1TdoNnao5AWwUJl9kkNpV891QvMdY',
+    authDomain: 'portfolio-10g.firebaseapp.com',
+    messagingSenderId: '51483704937',
+    measurementId: 'G-4CHK10FB3Q',
+  },
+};
